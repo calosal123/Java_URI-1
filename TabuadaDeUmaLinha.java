372ff32459fad0dccfc04f1c5b3d64777b63d6d8
professorisidro <isidro@professorisidro.com.br>
2020-08-20 09:52:19 -0300

    continue e tabuadad de 1linha

---
+++ TabuadaDeUmaLinha.java
@@ -0,0 +1,6 @@
+import java.util.Scanner;
+public class TabuadaDeUmaLinha{
+    public static void main(String args[]){
+        for (int cont=0, num = (new Scanner(System.in)).nextInt(); cont<=10; System.out.println(num + " x "+cont+" = "+ (num * cont++)));
+    }
+}
